a6482afaf701d64040ff0295f09e45a0b4e88e10
Aivan Monceller <aivan@monceller.com>
2016-12-07 02:16:27 +0800

    convert to simple login

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,7 +1,12 @@
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
 
 const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/civxvca7l0ggn01982h17k54x'
+  uri: 'https://api.graph.cool/simple/v1/civxvca7l0ggn01982h17k54x',
+  opts: {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('auth_token')}`
+    }
+  }
 });
 
 const client = new ApolloClient({
